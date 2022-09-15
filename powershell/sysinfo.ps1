eb4fcf59d5b8f0923f0353febd647e6a8674bc9d
Strebejj <112003411+Strebejj@users.noreply.github.com>
2022-09-15 11:29:19 -0400

    Add files via upload
    PowerShell files

---
+++ powershell/sysinfo.ps1
@@ -0,0 +1,25 @@
+﻿$IP = getIP
+$UserName = UserName
+$Hostname = hostname
+$Version = Version
+$DayOfWeek = DayOfWeek
+$Date = Get-Date -Format "MM/dd/yyyy"
+
+function DayOfWeek{
+(Date).DayOfWeek
+}
+
+function getIP{
+    (get-netipaddress).ipv4address | Select-String "192*"
+}
+
+function UserName{
+(Get-LocalUser) | Select-String "Adm*"
+}
+
+
+
+$Report = "This machine's IP is $IP. User is $UserName. Hostname is $hostname. PowerShell 
+Version $Version. Today's Date is $DayOfWeek, $Date."
+
+ Write-Host("$Report")
