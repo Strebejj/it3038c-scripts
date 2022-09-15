fd2a21f6c6368053fc099b5926e59e64f610a24b
Strebejj <112003411+Strebejj@users.noreply.github.com>
2022-09-15 12:15:41 -0400

    PowerShell Files

---
+++ powershell/Lab3_Email.ps1
@@ -0,0 +1,30 @@
+$From = "strebeljj@gmail.com"
+$To = "reedws@ucmail.uc.edu"
+$Cc = "strebejj@mail.uc.edu"
+$Attachment = " "
+$Subject = "IT3038C Windows SysInfo"
+$Body = "This machine's IP is $IP. User is $UserName. Hostname is $hostname. PowerShell Version $Version. Today's Date is $DayOfWeek, $Date."
+$SMTPServer = "smtp.gmail.com"
+$SMTPPort = "587"
+$IP = getIP
+$UserName = UserName
+$Hostname = hostname
+$Version = $PSVersionTable.PSVersion
+$DayOfWeek = DayOfWeek
+$Date = Get-Date -Format "MM/dd/yyyy"
+
+
+function DayOfWeek{
+(Date).DayOfWeek
+}
+
+function getIP{
+(get-netipaddress).ipv4address | Select-String "192*"
+}
+
+function UserName{
+(Get-LocalUser) | Select-String "Adm*"
+}
+
+
+Send-MailMessage -To "$To" -Cc "$Cc" -From "$From" -Subject $Subject -Body $Body -SmtpServer $SMTPServer -port $SMTPPort -UseSSL -Credential (Get-Credential)
