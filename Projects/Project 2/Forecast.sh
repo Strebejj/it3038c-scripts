3309a77417d90f17c6ea1a29ef1243cddeb6403f
strebejj <strebejj@mail.uc.edu>
2022-10-28 17:55:16 -0400

    Lab 8

--- Projects/Project 2/Forecast.sh
+++ Projects/Project 2/Forecast.sh
@@ -20,8 +20,11 @@ TngtTemp=$(echo $json|jq -r ."weather[0]|(.hourly[7].tempF)") #Tonight tempature
 TngtDesc=$(echo $json|jq -r ."weather[0]|(.hourly[7].weatherDesc[0].value)") #Tonight description variable, gives a description of what the weather will be like at 9:00 P.M. on the day the script is ran.
 TngtCloud=$(echo $json|jq -r ."weather[0]|(.hourly[7].cloudcover)") #Tonight cloud coverage variable, tells what percent of the sky will be covered by clouds at 9:00 P.M. on the day the script is ran.
 TngtHumid=$(echo $json|jq -r ."weather[0]|(.hourly[7].humidity)") #Tonight humiditiy variable, tells what the humidity will be at 9:00 P.M. on the day the script is ran.
+TngtMoonPhase=$(echo $json|jq -r ."weather[0]|(.astronomy[0].moon_phase)")
+TngtMoonIllumination=$(echo $json|jq -r ."weather[0]|(.astronomy[0].moon_illumination)")
 
-Overall=$(( 100 - ($TngtCloud + $TngtHumid) / 2)) #Calculates the overall viewing quality.
+
+Overall=$(( 100 - 100 * ($TngtCloud + $TngtMoonIllumination / 2 + $TngtHumid / 3) / 188)) #Calculates the overall viewing quality.
 
 #If statement that assigns a descriptive value of great, average, or poor for the veiwing quality.
 if [[ $Overall > 70 ]]
@@ -35,10 +38,11 @@ else
 fi
 
 #These are the output commands.
-echo "Currently, the weather is $CurrTemp*F and $CurrDesc. The humidity is $CurrHumid% and the total cloud coverage of the sky is $CurrCloud%."
+echo "Currently, the weather is $CurrTemp*F and $CurrDesc. The total cloud coverage of the sky is $CurrCloud% and the humidity is $CurrHumid%."
 echo "At 9:00 P.M. the forecast is $TngtTemp*F and $TngtDesc. There will be $TngtCloud% of cloud coverage and the humidity level will be $TngtHumid%."
+echo "Tonight the moon will be in the $TngtMoonPhase phase and will be $TngtMoonIllumination% illuminated."
 echo "Overall the viewing quality $Overall%. I would classify this night as $Dignify night to observe."
 
 # Overall, I feel this script automates everything I would need to know to determine if it would be a good night to observe.
-# For the next phase of this project I would like to add some variables for the moon, such as the phase and illumination as it can effect observing. I also can see a if statment being created that will alert the user if it is a full moon or new moon.
+# In this phase of this project I added some variables for the moon, such as the phase and illumination as it can effect observing. I also added an if statment that alerts the user if it is a full moon or new moon.
 # In the final phase of this project I would like to add a color scale to the variable outputs and bold areas of the output to enhance readability.
